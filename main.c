8f62a4c5635af46e35e23626fa91cc8aa7da9d42
Sebastián Paruma <jsebastian.paruma@gmail.com>
2020-11-27 00:10:12 +0000

    [ADD] Bring old files back

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
-#include "functions.h"
+#include "shell.h"
 
 /**
- * main - Entry point.
+ * main - main function to execute the shell, deterninate
+ * if the shell going to run in interactive or no-interactive
+ * mode
  *
- * Return: Always 0. (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char *input, **av;
-	pid_t PID;
-	int status;
+	if (isatty(STDIN_FILENO))
+		sh_interactive();
+	else
+		sh_nointeractive();
 
-	while (1)
-	{
-		PID = fork();
-		if (PID == 0)
-		{
-			input = get_commands();
-			av = get_argv(input);
-			status = execve(av[0], av, environ);
-			if (status == -1)
-			{
-				free_array(av);
-				perror("Error:");
-			}
-			else
-				free_array(av);
-		}
-		else if (PID == -1)
-			exit(EXIT_FAILURE);
-		else
-			wait(NULL);
-	}
 	return (0);
 }
